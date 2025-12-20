6a170d72599de78ecc7060f5fb6d0732f63ec645
MENSTRUE <menstruesalvation@gmail.com>
2025-12-20 15:57:24 +0700

    change yml and add requiretment

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,32 +1,23 @@
 import pandas as pd
 import mlflow
 from sklearn.ensemble import RandomForestClassifier
-import os
 
-# 1. Konfigurasi Kredensial (Bypass Login Manual)
-OWNER = 'MENSTRUE'
-REPO_NAME = 'Eksperimen_SML_wafa_bila_syaefurokhman'
+# Setup tracking ke DagsHub (Username & Password diambil dari ENV GitHub Actions)
+mlflow.set_tracking_uri("https://dagshub.com/MENSTRUE/Eksperimen_SML_wafa_bila_syaefurokhman.mlflow")
 
-# Mengambil token dari GitHub Secrets yang sudah kamu pasang
-token = os.getenv("MLFLOW_TRACKING_PASSWORD")
-
-# Setup URI dan Environment secara manual agar GitHub Actions bisa login
-mlflow.set_tracking_uri(f"https://dagshub.com/{OWNER}/{REPO_NAME}.mlflow")
-os.environ['MLFLOW_TRACKING_USERNAME'] = OWNER
-os.environ['MLFLOW_TRACKING_PASSWORD'] = token
-
-# 2. Path Data
+# PATH DATA: Harus relatif terhadap lokasi file MLProject
+# Karena running di GitHub Actions, path-nya adalah:
 X_path = 'preprocessing/nearest_earth_object_preprocessing/X_train.csv'
 y_path = 'preprocessing/nearest_earth_object_preprocessing/y_train.csv'
 
 X_train = pd.read_csv(X_path)
 y_train = pd.read_csv(y_path)
 
-# 3. Autolog dan Training
-mlflow.autolog()
+# Aktifkan autolog agar tercatat otomatis ke Run yang dibuat oleh Workflow CI
+mlflow.sklearn.autolog()
 
-with mlflow.start_run(run_name="NASA_Asteroid_CI_Retraining"):
-    model = RandomForestClassifier(random_state=42)
-    model.fit(X_train, y_train.values.ravel())
+# LANGSUNG TRAINING (Tanpa start_run manual)
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train.values.ravel())
 
-    print("Modelling CI Selesai! Cek DagsHub sekarang.")
+print("Modelling CI Selesai!")
